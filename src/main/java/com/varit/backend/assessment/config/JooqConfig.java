73b6b320d87169a1df088380ef841b66bb642ab8
Varit C <varit.bond@gmail.com>
2024-05-09 00:24:15 +0700

    Improve jooq config

--- src/main/java/com/varit/backend/assessment/config/JooqConfig.java
+++ src/main/java/com/varit/backend/assessment/config/JooqConfig.java
@@ -1,22 +1,19 @@
 package com.varit.backend.assessment.config;
 
+import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DefaultDSLContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 @Configuration
+@RequiredArgsConstructor
 public class JooqConfig {
-    private final DataSource dataSource;
 
-    @Autowired
-    public JooqConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    private final DataSource dataSource;
 
     @Bean
     public DSLContext dslContext() {
